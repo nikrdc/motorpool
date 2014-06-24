751415d65aa1a3bdebe732a984511e9a4e963910
nikrdc <nikrdc@gmail.com>
2014-06-23 23:39:58 -0400

    added email configuration

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SENDER = 'Motorpool'
+    MAIL_SENDER = 'Motorpool'
 
     @staticmethod
     def init_app(app):
