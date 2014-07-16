b4ace1c27ce3f55fb2a496731a58de0cf71cdbc6
nikrdc <nikrdc@gmail.com>
2014-07-16 11:09:55 -0400

    unknown amount of work since last commit

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ class Driver(db.Model):
     name = db.Column(db.String(64))
     phone = db.Column(db.String(64))
     capacity = db.Column(db.Integer)
-    make_model = db.Column(db.String(64))
     car_color = db.Column(db.String(64))
-    datetime = db.Column(db.DateTime)
+    make_model = db.Column(db.String(64))
     location = db.Column(db.String(64))
+    datetime = db.Column(db.DateTime)
 
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     riders = db.relationship('Rider', backref='driver', lazy='dynamic')
@@ -86,10 +86,10 @@ class DriverForm(Form):
     phone = StringField('Phone number', validators = [Required(), Length(10)])
     capacity = IntegerField('Car capacity', validators = [Required(), 
                                                           NumberRange(1, 10)])
-    make_model = StringField('Car make and model', validators = [Required()])
     car_color = StringField('Car color', validators = [Required()])
-
-    directions = SelectMultipleField('What direction are you travelling in?',
+    make_model = StringField('Car make and model', validators = [Required()])
+    
+    directions = SelectMultipleField('Direction',
         choices=[('driving_there', 'I am driving there'), 
                  ('driving_back', 'I am driving back')],
         validators = [Required()])
@@ -123,6 +123,25 @@ def find(token):
         return False
     return data
 
+def create_driver(direction):
+    if direction == 'driving_there':
+        direction_filler = True
+        location_filler = 'leaving_from'
+        datetime_filler = 'leaving_at'
+    elif direction == 'driving_back':
+        direction_filler = False
+        location_filler = 'going_to'
+        datetime_filler = 'going_at'
+    driver = Driver(going_there = direction_filler,
+                    name = form.name.data,
+                    phone = form.phone.data,
+                    capacity = form.capacity.data,
+                    car_color = form.car_color.data,
+                    make_model = form.make_model.data,
+                    location = getattr(form, location_filler).data,
+                    datetime = getattr(form, datetime_filler).data)
+    return driver
+
 
 # Errors
 
@@ -140,7 +159,7 @@ def internal_server_error(e):
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    form = AddEventForm()
+    form = EventForm()
     if form.validate_on_submit():
         event = Event(name = form.name.data)
         db.session.add(event)
@@ -163,15 +182,30 @@ def show_event(event_token):
 @app.route('/<event_token>/<driver_token>', methods = ['GET', 'POST'])
 def show_driver(event_token, driver_token):
     event = Event.query.get(find(event_token))
-    driver = Driver.query.get(driver(event_token))
-    form = DriverForm()
+    driver = Driver.query.get(find(driver_token))
 
 
-# AIFHIDAFISDF
 @app.route('/<event_token>/add', methods = ['GET', 'POST'])
-def add_driver(event_token, driver_type):
+def add_driver(event_token):
     form = DriverForm()
     event = Event.query.get(find(event_token))
+    if form.validate_on_submit():
+        if len(form.directions.data) == 2:
+            driver_there = create_driver(form.directions.data[0])
+            driver_back = create_driver(form.directions.data[1])
+            event.drivers.append(driver_there, driver_back)
+            db.session.add(driver_there, driver_back)
+        elif len(form.directions.data) == 1:
+            driver = create_driver(form.directions.data[0])
+            event.drivers.append(driver)
+            db.session.add(driver)
+        else:
+            return False
+        db.session.commit()
+        return redirect(url_for('show_event', event_token = event_token))
+    return render_template('add_driver.html', form = form)
+
+
 
 
 @app.route('/<event_token>/<driver_token>/add', methods = ['GET', 'POST'])
