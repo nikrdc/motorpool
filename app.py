8333f783d534fa7f9aa7b6f3d8c82b9c46bcb246
nikrdc <nikrdc@gmail.com>
2014-07-22 13:54:31 -0400

    completed event page

--- app.py
+++ app.py
@@ -116,16 +116,12 @@ class RiderForm(Form):
 
 # Helpers
 
-event_serializer = URLSafeSerializer(app.config['SECRET_KEY'], 
-                                     salt = 'event-salt')
+serializer = URLSafeSerializer(app.config['SECRET_KEY'])
 
-driver_serializer = URLSafeSerializer(app.config['SECRET_KEY'], 
-                                      salt = 'driver-salt')
-
-def generate_token(self, serializer):
+def generate_token(self):
     return serializer.dumps(self.id)
 
-def find(token, serializer):
+def find(token):
     try:
         data = serializer.loads(token)
     except:
@@ -141,6 +137,8 @@ def create_driver(form, direction, event):
         direction_filler = False
         location_filler = 'going_to'
         datetime_filler = 'going_at'
+    else:
+        return False
     driver = Driver(going_there = direction_filler,
                     name = form.name.data,
                     phone = form.phone.data,
@@ -174,53 +172,60 @@ def index():
         event = Event(name = form.name.data)
         db.session.add(event)
         db.session.commit()
-        event_token = generate_token(event, event_serializer)
+        event_token = generate_token(event)
+        flash('Share this page\'s URL with other attendees!')
         return redirect(url_for('show_event', event_token = event_token))
-    return render_template('index.html', form = form)
+    else:
+        return render_template('index.html', form = form)
 
 
 @app.route('/<event_token>')
 def show_event(event_token):
-    if find(event_token, event_serializer):
-        event = Event.query.get(find(event_token, event_serializer))
-        return render_template('event.html', event = event)
-    flash('The event you\'re looking for doesn\'t exist!')
-    return redirect(url_for('index'))
+    if find(event_token):
+        event = Event.query.get(find(event_token))
+        return render_template('event.html', event = event, 
+                               event_token = event_token)
+    else:
+        flash('The event you\'re looking for doesn\'t exist!')
+        return redirect(url_for('index'))
 
 
 # AIFHIDAFISDF
-@app.route('/<event_token>/<driver_token>', methods = ['GET', 'POST'])
-def show_driver(event_token, driver_token):
+@app.route('/<event_token>/<driver_id>', methods = ['GET', 'POST'])
+def show_driver(event_token, driver_id):
     event = Event.query.get(find(event_token))
-    driver = Driver.query.get(find(driver_token))
 
 
 @app.route('/<event_token>/add', methods = ['GET', 'POST'])
 def add_driver(event_token):
-    form = DriverForm()
-    event = Event.query.get(find(event_token, event_serializer))
-    if form.validate_on_submit():
-        directions = form.directions.data
-        directions_length = len(directions)
-        if directions_length == 2:
-            driver_there = create_driver(form, directions[0], event)
-            driver_back = create_driver(form, directions[1], event)
-            db.session.add_all([driver_there, driver_back])
-        elif directions_length == 1:
-            driver = create_driver(form, directions[0], event)
-            db.session.add(driver)
+    if find(event_token):
+        form = DriverForm()
+        event = Event.query.get(find(event_token))
+        if form.validate_on_submit():
+            directions = form.directions.data
+            directions_length = len(directions)
+            if directions_length == 2:
+                driver_there = create_driver(form, directions[0], event)
+                driver_back = create_driver(form, directions[1], event)
+                db.session.add_all([driver_there, driver_back])
+            elif directions_length == 1:
+                driver = create_driver(form, directions[0], event)
+                db.session.add(driver)
+            else:
+                return False
+            db.session.commit()
+            return redirect(url_for('show_event', event_token = event_token))
         else:
-            return False
-        db.session.commit()
-        return redirect(url_for('show_event', event_token = event_token))
-    return render_template('add_driver.html', form = form)
+            return render_template('add_driver.html', form = form)
+    else:
+        abort(404)
 
 
-@app.route('/<event_token>/<driver_token>/add', methods = ['GET', 'POST'])
-def add_rider(event_token, driver_token):
-    event = Event.query.get(find(event_token, event_serializer))
-    driver = Driver.query.get(find(driver_token, driver_serializer))
-    if driver in event.drivers:
+@app.route('/<event_token>/<driver_id>/add', methods = ['GET', 'POST'])
+def add_rider(event_token, driver_id):
+    event = Event.query.get(find(event_token))
+    driver = Driver.query.get(driver_id)
+    if len(driver.riders.all()) < driver.capacity - 1:
         form = RiderForm()
         if form.validate_on_submit():
             rider = Rider(name = form.name.data,
@@ -229,8 +234,11 @@ def add_rider(event_token, driver_token):
             db.session.add(rider)
             db.session.commit()
             return redirect(url_for('show_event', event_token = event_token))
-        return render_template('add_rider.html', form = form, driver = driver)
-    return False
+        else:
+            return render_template('add_rider.html', form = form, driver = driver)
+    else:
+        flash('There isn\'t any space left on that ride!')
+        return redirect(url_for('show_event', event_token = event_token))
 
 
 
